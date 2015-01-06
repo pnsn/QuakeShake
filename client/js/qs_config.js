444dd1872f545badc24e4f9e6453b2371a58bb0d
Jon Connolly <jontconnolly@gmail.com>
2015-01-05 23:17:42 -0800

    remove config stuff from client

---
+++ client/js/qs_config.js
@@ -0,0 +1,77 @@
+$(function(){  
+
+var colors ={
+    shBlue: '#00102F',
+    shGreen: '#5CBD59',
+    shDarkBlue: "#061830"
+};
+
+
+
+
+canvasConfig =function canvasConfig(){
+  this.pixPerSec      = 10;  //10 pix/sec = samples second i.e. the highest resolution we can display
+  this.timeWindowSec  = 102.4;
+  this.timeStep       = 1000/this.pixPerSec;
+  this.channelHeight  = 200; //how many pix for each signal
+  this.height         = null;
+  this.width          = this.timeWindowSec*this.pixPerSec;
+  this.buffer         = null;
+  this.axisColor      = "#000";
+	this.lineWidth      = 1;
+	this.tickInterval   = 10*1000;
+  this.starttime      = Date.now()*1000; //make these real big and real small so they will be immediately overwritten
+  this.endtime        = 0;
+  this.startPixOffset = this.width; //starttime pixelOffset
+  this.lastTimeFrame  = null; // track the time of the last time frame(left side of canvas this will be incremented each interval)
+  this.canvasElement  = document.getElementById("quakeShake");
+  this.localTime      = true;
+  this.scale          = 1; 
+  this.realtime       = true; //realtime will fast forward if tail of buffer gets too long.
+  this.scroll         = null; //sets scrolling
+};
+              
+              
+channels = [
+    new Scnl({
+    sta: "HWK1",  
+    chan: "HNZ", 
+    net: "UW", 
+    loc: "--", 
+    max: null,
+    position: 0, //from top 0 is highest
+    lineColor: colors.shBlue
+  }),
+      new Scnl({
+      sta: "HWK2",  
+      chan: "HNZ", 
+      net: "UW", 
+      loc: "--", 
+      max: null,
+      position: 1, //from top 0 is highest
+      lineColor: colors.shGreen
+    }),
+      new Scnl({
+      sta: "HWK3",  
+      chan: "HNZ", 
+      net: "UW", 
+      loc: "--", 
+      max: null,
+      position: 2, //from top 0 is highest
+      lineColor: colors.shDarkBlue
+    })
+];
+
+function Scnl(scnl){
+  //this.buf = [];
+  this.sta = scnl.sta;
+  this.chan = scnl.chan;
+  this.net = scnl.net;
+  this.loc = scnl.loc;
+  var loc = (!this.loc || this.loc == "--" || this.loc =="") ?  "" : ("_" + this.loc);
+  this.key =  this.sta.toLowerCase() + "_" + this.chan.toLowerCase() + "_" + this.net.toLowerCase() + loc;
+  this.lineColor =  scnl.lineColor;
+  this.position = scnl.position;
+}
+
+});
--- client/js/quakeShakeClient.js
+++ client/js/quakeShakeClient.js
@@ -1,35 +1,7 @@
 //client side of quakeShake 
 $(function(){  
-  var canvas = new Canvas();
-var channels = [
-    new Scnl({
-    sta: "HWK1",  
-    chan: "HNZ", 
-    net: "UW", 
-    loc: "--", 
-    max: null,
-    position: 0, //from top 0 is highest
-    lineColor: canvas.colors.shBlue
-  }),
-      new Scnl({
-      sta: "HWK2",  
-      chan: "HNZ", 
-      net: "UW", 
-      loc: "--", 
-      max: null,
-      position: 1, //from top 0 is highest
-      lineColor: canvas.colors.shGreen
-    }),
-      new Scnl({
-      sta: "HWK3",  
-      chan: "HNZ", 
-      net: "UW", 
-      loc: "--", 
-      max: null,
-      position: 2, //from top 0 is highest
-      lineColor: canvas.colors.shDarkBlue
-    })
-];
+  var canvas = new Canvas(new canvasConfig);
+
    $("#playback-slider").slider({
      slide: function(e, ui){
 
@@ -100,20 +72,34 @@ $("#button-realtime").click(function(){
   //end socket stuff
 
  
-  // the buf is initialized with a length = canvas.width
+  
+  //initial params that should be consistent across all channels on page
+  function Canvas(config){
+    this.pixPerSec      = config.pixPerSec;  //10 pix/sec = samples second i.e. the highest resolution we can display
+    this.timeWindowSec  = config.timeWindowSec;
+    this.timeStep       = config.timeStep; 
+    this.channelHeight  = config.channelHeight; //how many pix for each signal
+    this.height         = config.height;
+    this.width          = config.width;
+    this.buffer         = config.buffer;
+    this.axisColor      = config.axisColor;
+		this.lineWidth      = config.lineWidth;
+		this.tickInterval   = config.tickInterval;
+    this.starttime      = config.starttime; //make these real big and real small so they will be immediately overwritten
+    this.endtime        = config.endtime;
+    this.startPixOffset = config.startPixOffset; //starttime pixelOffset
+    this.lastTimeFrame  = config.lastTimeFrame; // track the time of the last time frame(left side of canvas this will be incremented each interval)
+    this.canvasElement  = config.canvasElement;
+    this.localTime      = config.localTime;
+    this.scale          = config.scale; 
+    this.realtime       = config.realtime; //realtime will fast forward if tail of buffer gets too long.
+    this.scroll         = config.scroll; //sets scrolling
+  };
+  
+  
+  
   // incoming data are appended to buf
   // drawing is done from left to right (old to new)
-  function Scnl(scnl){
-    //this.buf = [];
-    this.sta = scnl.sta;
-    this.chan = scnl.chan;
-    this.net = scnl.net;
-    this.loc = scnl.loc;
-    var loc = (!this.loc || this.loc == "--" || this.loc =="") ?  "" : ("_" + this.loc);
-    this.key =  this.sta.toLowerCase() + "_" + this.chan.toLowerCase() + "_" + this.net.toLowerCase() + loc;
-    this.lineColor =  scnl.lineColor;
-    this.position = scnl.position;
-  }
   
   //buffer will be of form:
   
@@ -126,35 +112,6 @@ $("#button-realtime").click(function(){
   //  }
   //        ....
   //}
-  
-  //initial params that should be consistent across all channels on page
-  function Canvas(){
-    this.pixPerSec         = 10;  //10 pix/sec = samples second i.e. the highest resolution we can display
-    this.timeWindowSec  = 102.4;
-    this.timeStep = 1000/this.pixPerSec;
-    this.channelHeight  = 200; //how many pix for each signal
-    this.height         = null;
-    this.width          = this.timeWindowSec*this.pixPerSec;
-    this.buffer         = null;
-    this.axisColor      = "#000";
-		this.lineWidth      = 1;
-		this.tickInterval   = 10*1000;
-    this.starttime      = Date.now()*1000; //make these real big and real small so they will be immediately overwritten
-    this.endtime        = 0;
-    this.colors         =  {
-                            shBlue: '#00102F',
-  	                        shGreen: '#5CBD59',
-  	                        shDarkBlue: "#061830"
-                          };
-    this.startPixOffset = this.width; //starttime pixelOffset
-    this.lastTimeFrame= null; // track the time of the last time frame(left side of canvas this will be incremented each interval)
-    this.canvasElement = document.getElementById("quakeShake");
-    this.localTime = true;
-    this.scale = 1; 
-    this.realtime = true; //realtime will fast forward if tail of buffer gets too long.
-    this.scroll = null; //sets scrolling
-  };
-  
   
   //called when new data arrive. Funciton independently from 
   // drawSignal method which is called on a sampRate interval
